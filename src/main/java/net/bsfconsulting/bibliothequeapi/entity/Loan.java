037dfb1dd3c6de217bb95cf6f575bb8f8f744f23
FaresBssm <159578369+FaresBssm@users.noreply.github.com>
2025-01-17 22:32:51 +0100

    Update Loan.java
    wip

--- src/main/java/net/bsfconsulting/bibliothequeapi/entity/Loan.java
+++ src/main/java/net/bsfconsulting/bibliothequeapi/entity/Loan.java
@@ -1,6 +1,5 @@
 package net.bsfconsulting.bibliothequeapi.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,6 +27,5 @@ public class Loan {
     private User user;
     @ManyToOne
     @JoinColumn(name = "book_id", nullable = false)
-    @JsonBackReference
     private Book book;
 }
